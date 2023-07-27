f8406d79cdcbc004d6854f88df9e3a6ec4629bca
Ramon Ivandy Setiawan <ramonivandy21@gmail.com>
2023-07-27 10:24:58 +0700

    Time: 28 ms (34.04%), Space: 4.3 MB (100%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.go
+++ 0009-palindrome-number/0009-palindrome-number.go
@@ -1,8 +1,10 @@
 func isPalindrome(x int) bool {
-    
+    if x < 0 {
+        return false
+    }
+
     temp := x
     reversed := 0
-    
     for temp > 0 {
         reversed = reversed*10 + temp%10
         temp /= 10

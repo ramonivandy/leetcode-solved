f901321c2f3c1188bafbfffa58fba390f879af31
Ramon Ivandy Setiawan <ramonivandy21@gmail.com>
2023-08-03 15:51:43 +0700

    Time: 10 ms (42.67%), Space: 4.4 MB (98.76%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.go
@@ -0,0 +1,13 @@
+func removeDuplicates(nums []int) int {
+    idx := 0
+    
+    for i := 0; i < len(nums); i++{
+        if nums[idx] != nums[i]{
+            idx++
+            nums[idx] = nums[i]
+        }
+    }
+
+    return idx + 1
+
+}

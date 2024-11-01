074cf96a5185c5175f8e0a5370a68f84c0442ea6
Ramon Ivandy Setiawan <ramonivandy21@gmail.com>
2024-11-02 05:53:34 +0700

    Time: 80 ms (5.06%), Space: 60 MB (5.14%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    if (l1.next == null && l2 == null) {
+        return [0]
+    }
+
+    let index = 1;
+    let res = new ListNode(index);
+    let returnRes = res;
+
+    let carrier = 0;
+    while (l1 || l2) {
+        let sum = 0;
+        let l1Val = l1 ? l1.val : 0;
+        let l2Val = l2 ? l2.val : 0;
+
+        console.log(`${l1Val} || ${l2Val}`)
+
+        sum = l1Val + l2Val;
+
+        if(carrier > 0){
+            sum += carrier;
+            carrier = 0;
+        }
+
+        if (sum > 9) {
+            carrier = 1;
+            res.val = sum % 10;
+        } else {
+            res.val = sum;
+        }
+
+        if(l1 && l1.next || l2 && l2.next){
+            index++
+            res.next = new ListNode(index)
+            res = res.next
+        }
+
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+
+        if(!l1 && !l2 && carrier > 0){
+            index++
+            res.next = new ListNode(index)
+            res.next.val = carrier
+        }
+    }
+
+    return returnRes;
+};

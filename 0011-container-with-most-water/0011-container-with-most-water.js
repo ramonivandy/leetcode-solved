6eb8b9c5f8e0517d30e1f7276cf7ed4c89bf4699
Ramon Ivandy Setiawan <ramonivandy21@gmail.com>
2024-11-05 11:35:34 +0700

    Time: 1 ms (95.96%), Space: 57.5 MB (57.4%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1
+    let maxArea = 0;
+
+    while (left < right) {
+        let area = (right - left) * (Math.min(height[left], height[right]));
+        maxArea = Math.max(maxArea, area);
+
+        if(height[left] < height[right]) {
+            left++
+        } else {
+            right--
+        }
+    }
+
+    return maxArea
+};

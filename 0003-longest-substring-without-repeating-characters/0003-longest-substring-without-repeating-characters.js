57d9f6cb1260d1951d690293b65813718b94f986
Ramon Ivandy Setiawan <ramonivandy21@gmail.com>
2024-11-18 14:16:03 +0700

    Time: 10 ms (69.51%), Space: 54.6 MB (59.48%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let left = 0;
+    let charSet = new Set();
+    let maxLength = 0;
+
+    for(let right = 0; right < s.length; right++){
+
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++
+        }
+    
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1)
+    }
+
+    return maxLength;
+
+};
